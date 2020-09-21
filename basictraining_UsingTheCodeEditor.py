f2d6ac8b23183681437a2768e84667121b07d41d
Maxed-Red <forresthpatrick@gmail.com>
2020-09-20 20:21:55 -0400

    Add files via upload

---
+++ basictraining_UsingTheCodeEditor.py
@@ -0,0 +1,12 @@
+#Basic Training: Using the Code Editor
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+if __name__ == '__main__':
+    app.run()
